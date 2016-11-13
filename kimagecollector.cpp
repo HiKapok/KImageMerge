50987b7206e8c1758c33679a7d3eac5811aed6d0
e <wangchangan@yeah.net>
2016-11-13 13:01:01 +0800

    fix bugs

--- kimagecollector.cpp
+++ kimagecollector.cpp
@@ -13,23 +13,54 @@
 
 #include "krawdblintimg.h"
 
+//class KStringCompare : public std::binary_function<std::string, std::string, bool> {
+//public:
+//    KStringCompare()
+//        : re((QObject::tr("_([0-9.]{4,6})_"))){}
+//    result_type operator()(first_argument_type lhs, second_argument_type rhs){
+//        QRegularExpressionMatch lmatch = re.match(lhs.c_str());
+//        QRegularExpressionMatch rmatch = re.match(rhs.c_str());
+
+//        if (lmatch.hasMatch() && rmatch.hasMatch()) {
+//            return lmatch.captured(1).toInt() < rmatch.captured(1).toInt();
+//        }
+//        return false;
+//    }
+//private:
+//    QRegularExpression re;
+//};
+
+//QRegularExpression re((QObject::tr("_(\\-{0,1}[0-9]{1,2}\\.[0-9]{3})_(\\-{0,1}[0-9]{1,2}\\.[0-9]{3})\\.DAT")));
+//QRegularExpressionMatch lmatch = re.match("AutoLvdsImage_02_-10.000_2.500.DAT");
+
+//if (lmatch.hasMatch()) {
+//    qDebug()<< lmatch.captured(2);
+//}
 class KStringCompare : public std::binary_function<std::string, std::string, bool> {
 public:
     KStringCompare()
-        : re((QObject::tr("_([0-9]{2,3})_"))){}
+        : re((QObject::tr("_(\\-{0,1}[0-9]{1,2}\\.[0-9]{3})_(\\-{0,1}[0-9]{1,2}\\.[0-9]{3})\\.DAT"))){}
     result_type operator()(first_argument_type lhs, second_argument_type rhs){
         QRegularExpressionMatch lmatch = re.match(lhs.c_str());
         QRegularExpressionMatch rmatch = re.match(rhs.c_str());
 
         if (lmatch.hasMatch() && rmatch.hasMatch()) {
-            return lmatch.captured(1).toInt() < rmatch.captured(1).toInt();
+            if(lmatch.captured(2).toDouble() > rmatch.captured(2).toDouble()) return true;
+            else{
+                if(lmatch.captured(2).toDouble() == rmatch.captured(2).toDouble()){
+                    if(lmatch.captured(1).toDouble() <= rmatch.captured(1).toDouble()) return true;
+                    else return false;
+                }else return false;
+
+            }
+            //return (lmatch.captured(2).toDouble() > rmatch.captured(2).toDouble() ) && (lmatch.captured(1).toInt() < rmatch.captured(1).toInt());
+            //return lmatch.captured(1).toInt() < rmatch.captured(1).toInt();
         }
         return false;
     }
 private:
     QRegularExpression re;
 };
-
 KImageCollector::KImageCollector(/*QWidget *pWidget*/)
     //: m_pMainWidget(pWidget)
 {
@@ -40,6 +71,7 @@ KImageCollector::KImageCollector(/*QWidget *pWidget*/)
 bool KImageCollector::prepare_lists(QString targetDir){
     QDir thisDir(targetDir);
     QStringList fileLists = thisDir.entryList(QStringList()<<QObject::tr("*.DAT"), QDir::Files | QDir::NoSymLinks | QDir::Readable, QDir::Name);
+
 
     if(fileLists.empty()) return false;
     m_vecFileLists.clear();
--- main.cpp
+++ main.cpp
@@ -7,11 +7,15 @@
 
 #include <QDebug>
 
-
+#include <QRegularExpression>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    //QRegularExpression re((QObject::tr("_(\\-{0,1}[0-9]{1,2}\\.[0-9]{3})_")));
+    //QRegularExpression re((QObject::tr("_(\\-{0,1}[0-9]{1,2}\\.[0-9]{3})\\.DAT")));
+
     MainWindow wnd;
     wnd.show();
     return app.exec();
